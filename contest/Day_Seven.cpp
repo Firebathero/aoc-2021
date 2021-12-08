c6327cc0bc7e6035e7421b45fbf3477df8df74e0
Ryan Furton <ryanfurton@gmail.com>
2021-12-08 09:56:06 -0500

    Create Day_Seven.cpp

---
+++ contest/Day_Seven.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define arr array
+typedef long long ll;
+using namespace std;
+#define mp make_pair
+#define pb push_back
+#define pob pop_back()
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+int n;
+string s,t;
+vector<int>v;
+
+int main()
+{
+    freopen("input", "r", stdin);
+    cin >> s;
+    int prev = 0;
+    for (int i = 0; i < s.length(); ++i)
+        if(s[i]==',') {
+            t = s.substr(prev,i-prev);
+            v.push_back(stoi(t));
+            prev = i+1;
+        }
+    int ans = 0, res = 0;
+    v.push_back(stoi(s.substr(prev, s.length()-prev)));
+    sort(v.begin(), v.end());
+    for (int d : v) ans += d;
+    int mean = ans/v.size();
+    for (int d : v) {
+        d = abs(mean-d);
+        res += (d*(d+1))/2; 
+    }
+    cout << res;
+}
